d00de8e3ac0b7db3daff3dc217e43526e19349ef
Franco Dalla Via Oliveros <francodvo@gmail.com>
2026-01-25 04:55:32 -0300

    First functional commit

---
+++ src/templates/build.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/bash
+
+# Move current directory to source code (real path is replaced at runtime, leave this as is)
+cd "{{SOURCE_CODE}}"
+
+#===================================================#
+# ↓ ↓ ↓  Write your build instructions here!  ↓ ↓ ↓ #
